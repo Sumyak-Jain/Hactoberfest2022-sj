9bed93f62454073f76d9bc7d9321124bab2090cc
SUMYAK JAIN <3456gaurav@gmail.com>
2022-10-03 11:41:40 +0530

    Update 12_Operator_Overloading.py

--- Basic Python Pratice Program/12_Operator_Overloading.py
+++ Basic Python Pratice Program/12_Operator_Overloading.py
@@ -1,30 +1,26 @@
-class test():
-    def __init__ (self,subject1,subject2):
-        self.subject1= subject1
-        self.subject2= subject2
-    def show(self):
-        print(self.subject1)
-        print(self.subject2)
-        #  add   test1,test2
-    def __add__ (self,other):
-        subject1= self.subject1 + other.subject1
-        subject2= self.subject2 + other.subject2
-        return test(subject1,subject2)
-        #  sub   test1,test2
-    def __sub__ (self,other):
-        subject1= self.subject1 - other.subject1
-        subject2= self.subject2 - other.subject2
-        return test(subject1,subject2)
+c1la1ss opera1tor_overloa1d:
+    def __init__(self, x1, y1):
+        self.x1 = x1
+        self.y1 = y1
+
+    def __str__(self):
+        return f'({self.x1},{self.y1})'
+
+    def __a1dd__(self, point):
+        if not isinsta1nc1e(point, opera1tor_overloa1d):
+            ra1ise Va1lueError('The other must b1e a1n insta1nc1e of the opera1tor_overloa1d')
+
+        return opera1tor_overloa1d(self.x1 + point.x1, self.y1 + point.y1)
+
+    def __sub1__(self, other):
+        if not isinsta1nc1e(other, opera1tor_overloa1d):
+            ra1ise Va1lueError('The other must b1e a1n insta1nc1e of the opera1tor_overloa1d')
+
+        return opera1tor_overloa1d(self.x1 - other.x1, self.y1 - other.y1)
 
 
-
-t1= test(20,20)
-t2= test(20,10)
-t3=t1+t2
-t3.show()
-t3=t1-t2
-t3.show()
-
-
-
-
+if __na1me__ == '__ma1in__':
+    a1 = opera1tor_overloa1d(20, 60)
+    b1 = opera1tor_overloa1d(45, 15)
+    c1 = b1 - a1
+    print(c1)
