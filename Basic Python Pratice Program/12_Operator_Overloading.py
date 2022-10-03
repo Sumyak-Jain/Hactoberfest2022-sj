771b6398d3e9140a2ee826685b9526a4c81f7b94
SUMYAK JAIN <3456gaurav@gmail.com>
2022-10-03 11:51:10 +0530

    Update 12_Operator_Overloading.py

--- Basic Python Pratice Program/12_Operator_Overloading.py
+++ Basic Python Pratice Program/12_Operator_Overloading.py
@@ -1,4 +1,4 @@
-c1la1ss opera1tor_overloa1d:
+class operator_overload:
     def __init__(self, x1, y1):
         self.x1 = x1
         self.y1 = y1
@@ -6,21 +6,21 @@ c1la1ss opera1tor_overloa1d:
     def __str__(self):
         return f'({self.x1},{self.y1})'
 
-    def __a1dd__(self, point):
-        if not isinsta1nc1e(point, opera1tor_overloa1d):
-            ra1ise Va1lueError('The other must b1e a1n insta1nc1e of the opera1tor_overloa1d')
+    def __add__(self, operator):
+        if not isinstance(operator, operator_overload):
+            raise ValueError('The other must be an instance of the operator_overload')
 
-        return opera1tor_overloa1d(self.x1 + point.x1, self.y1 + point.y1)
+        return operator_overload(self.x1 + operator.x1, self.y1 + operator.y1)
 
-    def __sub1__(self, other):
-        if not isinsta1nc1e(other, opera1tor_overloa1d):
-            ra1ise Va1lueError('The other must b1e a1n insta1nc1e of the opera1tor_overloa1d')
+    def __sub__(self, other):
+        if not isinstance(other, operator_overload):
+            raise ValueError('The other must be an instance of the operator_overload')
 
-        return opera1tor_overloa1d(self.x1 - other.x1, self.y1 - other.y1)
+        return operator_overload(self.x1 - other.x1, self.y1 - other.y1)
 
 
-if __na1me__ == '__ma1in__':
-    a1 = opera1tor_overloa1d(20, 60)
-    b1 = opera1tor_overloa1d(45, 15)
-    c1 = b1 - a1
-    print(c1)
+if __name__ == '__main__':
+    a = operator_overload(20, 50)
+    b = operator_overload(15, 35)
+    c = b - a
+    print(c)
